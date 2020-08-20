83796fd68c778cbf3e052e57fc67cc29c7ffccef
ibnuhaseem <32239499+ibnuhaseem@users.noreply.github.com>
2020-08-20 13:41:15 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1597904337,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/balon-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/karakter-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/particles.png",
+		"images/backgroundbygreggman-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tali16x-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
